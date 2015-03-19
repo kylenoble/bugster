4ab70a776d0bd92ed9cfa9b1cbd3c51c7bf2ffd3
kylenoble <kyle.nobl@gmail.com>
2015-03-19 17:53:01 -0400

    fixed email suggestion controller

--- app/controllers/email_suggestions_controller.rb
+++ app/controllers/email_suggestions_controller.rb
@@ -2,9 +2,8 @@ class EmailSuggestionsController < ApplicationController
 	def index
 		if user_signed_in?
     	render json: EmailSuggestion.terms_for_user(params[:term], current_user.org)
-    end
     elsif admin_signed_in?
-    	render json: EmailSuggestion.terms_for_admin(params[:term], current_user.org)
+    	render json: EmailSuggestion.terms_for_admin(params[:term])
     end
   end
 
