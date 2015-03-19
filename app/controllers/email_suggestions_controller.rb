0de70af8346a3766bf61e427c5fd636ec72ef80c
kylenoble <kyle.nobl@gmail.com>
2015-03-19 17:49:41 -0400

    fixed email suggestion issue

--- app/controllers/email_suggestions_controller.rb
+++ app/controllers/email_suggestions_controller.rb
@@ -1,7 +1,10 @@
 class EmailSuggestionsController < ApplicationController
 	def index
 		if user_signed_in?
-    	render json: EmailSuggestion.terms_for(params[:term], current_user.org)
+    	render json: EmailSuggestion.terms_for_user(params[:term], current_user.org)
+    end
+    elsif admin_signed_in?
+    	render json: EmailSuggestion.terms_for_admin(params[:term], current_user.org)
     end
   end
 
--- app/models/email_suggestion.rb
+++ app/models/email_suggestion.rb
@@ -7,6 +7,7 @@ class EmailSuggestion
       	1.upto(email.length - 1) do |n|
         	prefix = email[0, n]
         	$redis.zadd "email-suggestions-#{org}:#{prefix.downcase}", 1, email.downcase
+          $redis.zadd "email-suggestions:#{prefix.downcase}", 1, email.downcase
         end
       end
     end
@@ -17,13 +18,18 @@ class EmailSuggestion
       	1.upto(email.length - 1) do |n|
         	prefix = email[0, n]
         	$redis.zadd "email-suggestions-#{org}:#{prefix.downcase}", 1, email.downcase
+          $redis.zadd "email-suggestions:#{prefix.downcase}", 1, email.downcase
         end
       end
     end
   end
 
-  def self.terms_for(prefix, org)
+  def self.terms_for_user(prefix, org)
     puts "email-suggestions-#{org}:#{prefix.downcase}"
     $redis.zrevrange "email-suggestions-#{org}:#{prefix.downcase}", 0, 9
+  end
+  def self.terms_for_admin(prefix)
+    puts "email-suggestions:#{prefix.downcase}"
+    $redis.zrevrange "email-suggestions:#{prefix.downcase}", 0, 9
   end
 end
